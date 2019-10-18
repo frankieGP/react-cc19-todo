ce80b887933ff69360dfba505e2002d9187df758
Frankie Gavin <frankanthonygavin@gmail.com>
2019-10-18 13:04:37 -0700

    addss add todo item functionality

--- src/App.js
+++ src/App.js
@@ -38,15 +38,27 @@ class App extends Component {
   delTodo = (id) => {
     this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
   }
+
+  //Add Todo
+
+  addTodo = (title) => {
+    const newTodo = {
+      id: 4,
+      title: title,
+      completed: false
+    }
+
+    this.setState({ todos: [...this.state.todos, newTodo]}) 
+  }
+
   render() {
     return (
       <div className="App">
         <div className="container">
           <Header />
-          <AddTodo />
+          <AddTodo addTodo={this.addTodo}/>
           <Todos todos={this.state.todos} markComplete={this.markComplete}
-          delTodo={this.delTodo}
-          />
+           delTodo={this.delTodo}/>
         </div>
       </div>
     );
